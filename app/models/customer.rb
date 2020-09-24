f0230d888484bcf26344963e78ea9965d0ea1a33
Keith O Brien <kobrien2@tcd.ie>
2020-09-24 21:01:58 +0100

    Cleanup

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,4 @@
 class Customer < ActiveRecord::Base
   validates :name, presence: true
 
-  scope :radius, ->(radius) {
-    "SELECT *
-    FROM your_table
-    WHERE ST_Distance_Sphere(the_geom, ST_MakePoint(your_lon,your_lat)) <= radius_mi * 1609.34"
-
-  }
 end
