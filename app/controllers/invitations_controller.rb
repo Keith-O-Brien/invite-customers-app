954a276cdf88724f9e64bcc82d8c3ab4cd35cbf2
Keith O Brien <kobrien2@tcd.ie>
2020-09-24 18:51:31 +0100

    Customer service now returning customers

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,20 +1,16 @@
 class InvitationsController < ApplicationController
 
+
   def create
-    customer_data = File.read("app/assets/text_files/customer_list.txt")
-    puts "df"
-
-    customer_data.each do |data|
-      data = data.split(",")
-      id = data[1]
-      name = data[2]
-      longitude = data[3]
-      latitude = data[0]
-
-      # Customer.find_or_create_by(id: id)
-    end
+    customers = customer_service.read_customers
 
     redirect_to root_path
+  end
+
+  private
+
+  def customer_service
+    @customer_service ||= CustomerService.new
   end
 
 end
--- app/services/customer_service.rb
+++ app/services/customer_service.rb
@@ -1,10 +1,23 @@
 class CustomerService
-  def initialize(message)
-    @message = message
-  end
-
   def read_customers
-    data = File.read("/assets/text_files/customer_list.txt")
+    customers = []
+    customer_data = File.read("app/assets/text_files/customer_list.txt")
+    customer_data = customer_data.split(/\n+/)
 
+    customer_data.each do |data|
+      customer_poro = JSON.parse(data, object_class: OpenStruct)
+
+      customer = Customer.find_or_create_by(id: customer_poro.user_id)
+      customer.update_attributes(
+          longitude: customer_poro.longitude,
+          latitude: customer_poro.latitude,
+          name: customer_poro.name
+      )
+
+      customers << customer
+    end
+
+    customers
   end
+
 end
