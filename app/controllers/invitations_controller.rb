f35fe6e24f93dd79e56f2c239f31be3aec1cfc39
Keith O Brien <kobrien2@tcd.ie>
2020-09-26 09:54:03 +0100

    Fix for array issue

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,14 +1,7 @@
 class InvitationsController < ApplicationController
+  before_action :set_customers, only: :show
 
   def show
-    @customers = []
-    distance_km = 100
-
-    Customer.all.each do |customer|
-      if distance_service.within_distance(customer, distance_km)
-        @customers << customer
-      end
-    end
 
   end
 
@@ -19,6 +12,19 @@ class InvitationsController < ApplicationController
   end
 
   private
+
+  def set_customers
+    customer_ids = []
+    distance_km = 100
+
+    Customer.all.each do |customer|
+      if distance_service.within_distance(customer, distance_km)
+        customer_ids << customer.id
+      end
+    end
+
+    @customers = Customer.where(id: customer_ids).order("id ASC")
+  end
 
   def customer_service
     @customer_service ||= CustomerService.new
